1141021ce4bff3ea128554b1cc780edde34b02eb
CODJean <92735437+CODJean@users.noreply.github.com>
2024-11-14 20:38:31 -0500

    Update Segmentos.ino

--- ARDUINOESP32/Segmentos.ino
+++ ARDUINOESP32/Segmentos.ino
@@ -2,6 +2,7 @@
 SevSeg sevseg; //Instantiate a seven segment controller object
 #define BUTTON_PIN 5
 #define DEBUTTON_PIN 18
+#define REBUTTON_PIN 19
 int count = 0;
 int lastButtonState = HIGH;
 unsigned long lastDebounceTime = 0;
@@ -21,21 +22,30 @@ void setup() {
   sevseg.setBrightness(90);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(DEBUTTON_PIN, INPUT_PULLUP);
+  pinMode(REBUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
   int buttonState = digitalRead(BUTTON_PIN);
+  int rebuttonState = digitalRead(REBUTTON_PIN);
+  int debuttonState = digitalRead(DEBUTTON_PIN);
   // Verificar si el botón fue presionado
   if (buttonState == LOW && lastButtonState == HIGH && (millis() - lastDebounceTime) >debounceDelay) {
     lastDebounceTime = millis();
     count = (count + 1) % 10000; // Incrementar y reiniciar el contador al llegar a 9999
   }
   lastButtonState = buttonState;
-  int debuttonState = digitalRead(DEBUTTON_PIN);
   // Verificar si el botón fue presionado
   if (debuttonState == LOW && lastButtonState == HIGH && (millis() - lastDebounceTime) >debounceDelay) {
     lastDebounceTime = millis();
     count = (count - 1) % 10000; // Incrementar y reiniciar el contador al llegar a 9999
+  }
+  lastButtonState = debuttonState;
+    lastButtonState = buttonState;
+  // Verificar si el botón fue presionado
+  if (rebuttonState == LOW && lastButtonState == HIGH && (millis() - lastDebounceTime) >debounceDelay) {
+    lastDebounceTime = millis();
+    count = 0; // Incrementar y reiniciar el contador al llegar a 9999
   }
   lastButtonState = debuttonState;
   sevseg.setNumber(count); // Configura el número que se mostrará en el display
