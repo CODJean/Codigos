c3048897aee23773eb623ef3ecade0fef8537e68
CODJean <92735437+CODJean@users.noreply.github.com>
2024-11-20 21:51:45 -0500

    Create SMSTELE.ino

---
+++ ARDUINOESP32/SMSTELE.ino
@@ -0,0 +1,52 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+// Wifi network station credentials
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+// Telegram BOT Token (Get from Botfather)
+// Bot ID and chat ID for Telegram
+#define BOT_TOKEN "7696696928:AAHUB0L41QE6Jz2TzGNclGmkU0ABzqxKe8w"
+#define CHAT_ID "7841503338"
+
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+ // attempt to connect to Wifi network:
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Retrieving time: ");
+  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(100);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+
+  
+}
+
+void loop() {
+  bot.sendMessage(CHAT_ID, "Bot started up", "");
+  delay(1000);
+
+}
